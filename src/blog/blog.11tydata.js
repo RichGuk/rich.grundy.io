0f433b472304ae131549749fd030e787a87398eb
Rich Grundy <rich@grundy.io>
2020-10-09 16:04:33 +0100

    Added sitemap, robots.txt and some url tweaks.

--- src/blog/blog.11tydata.js
+++ src/blog/blog.11tydata.js
@@ -2,6 +2,8 @@ module.exports = {
   layout: "layouts/blog-entry",
   tags: "post",
   permalink: "/blog/{% if postSlug %}{{ postSlug }}{% else %}{{ title | slug }}{% endif %}/index.html",
+  "changeFreq": "monthly",
+  "priority": "0.9",
   eleventyComputed: {
     posts: data => data.collections.post
   }
