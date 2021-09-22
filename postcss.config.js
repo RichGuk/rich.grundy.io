baa6751cf8be73501e4773cd664b474b918d66ac
Rich Grundy <rich@grundy.io>
2021-09-22 12:17:49 +0100

    Chrome doesn't support types in image-set?

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   plugins: {
-    "postcss-import": {},
-    "postcss-preset-env": {},
-    "postcss-nested-ancestors": {},
-    "postcss-nested": {},
-    "autoprefixer": {},
-  }
+    'postcss-import': {},
+    'postcss-preset-env': {},
+    'postcss-nested-ancestors': {},
+    'postcss-nested': {},
+    autoprefixer: {},
+  },
 };
