4522143a381eb59554fa678365025753742c6bda
Rich Grundy <rich@grundy.io>
2021-10-11 22:10:34 +0100

    Code cleanup.

--- .eleventy.js
+++ .eleventy.js
@@ -1,105 +1,98 @@
-const fs = require('fs');
-const path = require('path');
-const htmlmin = require('html-minifier');
-const { DateTime } = require('luxon');
+const fs = require("fs")
+const path = require("path")
+const htmlmin = require("html-minifier")
 
-const manifestPath = path.resolve(__dirname, 'dist', 'assets-manifest.json');
-let manifest = {};
+const manifestPath = path.resolve(__dirname, "dist", "assets-manifest.json")
+let manifest = {}
 if (fs.existsSync(manifestPath)) {
-  manifest = JSON.parse(fs.readFileSync(manifestPath, { encoding: 'utf8' }));
+  manifest = JSON.parse(fs.readFileSync(manifestPath, { encoding: "utf8" }))
 }
-const filters = require('./utils/filters.js');
+const filters = require("./utils/filters.js")
 
-const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
-const pluginRss = require('@11ty/eleventy-plugin-rss');
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight")
+const pluginRss = require("@11ty/eleventy-plugin-rss")
 
 module.exports = (eleventyConfig) => {
-  const devMode = process.env.NODE_ENV !== 'production';
+  const devMode = process.env.NODE_ENV !== "production"
 
-  eleventyConfig.setDataDeepMerge(true);
+  eleventyConfig.setDataDeepMerge(true)
 
-  eleventyConfig.addPlugin(syntaxHighlight);
-  eleventyConfig.addPlugin(pluginRss);
+  eleventyConfig.addPlugin(syntaxHighlight)
+  eleventyConfig.addPlugin(pluginRss)
 
-  eleventyConfig.addLayoutAlias('default', 'layouts/pages.njk');
-  eleventyConfig.addLayoutAlias('pages', 'layouts/pages.njk');
+  eleventyConfig.addLayoutAlias("default", "layouts/pages.njk")
+  eleventyConfig.addLayoutAlias("pages", "layouts/pages.njk")
 
   eleventyConfig
-    .addPassthroughCopy({ 'src/assets/static': '/assets' })
-    .addPassthroughCopy({ 'src/assets/vendor': 'vendor' });
+    .addPassthroughCopy({ "src/assets/static": "/assets" })
+    .addPassthroughCopy({ "src/assets/vendor": "vendor" })
 
-  eleventyConfig.addShortcode('assetUrl', (name) => {
+  eleventyConfig.addShortcode("assetUrl", (name) => {
     if (!manifest[name]) {
-      return name;
+      return name
     }
-    return manifest[name];
-  });
+    return manifest[name]
+  })
 
   eleventyConfig.addShortcode(
-    'imageTag',
-    (src, desc = '') => {
+    "imageTag",
+    (src, desc = "") => {
       src = devMode ? `/assets/images/${src}` : `//media.rich.grundy.io/${src}`
       const file = `${path.dirname(src)}/${path.basename(src, path.extname(src))}`
-      return `<picture><source srcset="${file}.avif" type="image/avif" loading="lazy"><source srcset="${file}.webp" type="image/webp" loading="lazy"><img src="${src}" alt="${desc}" loading="lazy"></picture>`;
+      return `<picture><source srcset="${file}.avif" type="image/avif" loading="lazy"><source srcset="${file}.webp" type="image/webp" loading="lazy"><img src="${src}" alt="${desc}" loading="lazy"></picture>`
     }
-  );
+  )
 
-  eleventyConfig.addCollection('tagList', function (collection) {
-    let newTags = {};
-
-    collection.getAll().forEach((item) => {
-      if (item.data['tags']) {
-        filters
-          .withoutSpecialTags(item.data['tags'])
-          .forEach((tag) => {
-            if (!newTags[tag]) {
-              newTags[tag] = 0;
-            }
-
-            newTags[tag]++;
-          });
+  eleventyConfig.addCollection("tagList", function (collection) {
+    const tags = [...collection.getAll()].reduce((acc, item) => {
+      if (item.data.tags) {
+        filters.withoutSpecialTags(item.data.tags).forEach((tag) => {
+          acc[tag] ||= 0
+          acc[tag] += 1
+        })
       }
-    });
 
-    return Object.keys(newTags)
-      .sort()
-      .reduce((res, k) => {
-        k = filters.slug(k);
-        res[k] = newTags[k];
-        return res;
-      }, {});
-  });
+      return acc
+    }, {})
+
+    return Object.keys(tags).sort().reduce((acc, tag) => {
+      acc[tag] = { slug: filters.slug(tag), count: tags[tag] }
+      return acc
+    }, {})
+  })
 
   Object.keys(filters).forEach((name) => {
-    eleventyConfig.addFilter(name, filters[name]);
-  });
+    eleventyConfig.addFilter(name, filters[name])
+  })
 
-  eleventyConfig.setBrowserSyncConfig({ files: [
-    './dist/assets'
-  ] });
+  eleventyConfig.setBrowserSyncConfig({
+    files: [
+      "./dist/assets"
+    ]
+  })
 
   // Minify HTML in for the prod build.
   if (!devMode) {
-    eleventyConfig.addTransform('htmlmin', (content, outputPath) => {
-      if (outputPath.endsWith('.html')) {
-        let minified = htmlmin.minify(content, {
+    eleventyConfig.addTransform("htmlmin", (content, outputPath) => {
+      if (outputPath.endsWith(".html")) {
+        const minified = htmlmin.minify(content, {
           useShortDoctype: true,
           removeComments: true,
-          collapseWhitespace: true,
-        });
-        return minified;
+          collapseWhitespace: true
+        })
+        return minified
       }
-      return content;
-    });
+      return content
+    })
   }
 
   return {
     dir: {
-      input: 'src',
-      output: 'dist',
+      input: "src",
+      output: "dist"
     },
-    htmlTemplateEngine: 'njk',
-    markdownTemplateEngine: 'njk',
-    passthroughFileCopy: true,
-  };
-};
+    htmlTemplateEngine: "njk",
+    markdownTemplateEngine: "njk",
+    passthroughFileCopy: true
+  }
+}
--- utils/filters.js
+++ utils/filters.js
@@ -1,42 +1,43 @@
-const slugify = require('slugify');
-const { DateTime } = require('luxon');
+const slugify = require("slugify")
+const { DateTime } = require("luxon")
 
 const slugFilter = (str) => {
-  return slugify(str.replace(/\//g, '-'), {
+  return slugify(str.replace(/\//g, "-"), {
     remove: /[*+~.()'"!:@,?]/g,
-    replacement: '-',
-    lower: true,
-  });
-};
+    replacement: "-",
+    lower: true
+  })
+}
 
-const specialTags = new Set(['post', 'posts']);
-const withoutSpecialTags = (tags) => tags.filter((t) => t && !specialTags.has(t));
+const specialTags = new Set(["post", "posts"])
+const withoutSpecialTags = (tags) =>
+  tags.filter((t) => t && !specialTags.has(t))
 
 module.exports = {
   slug: slugFilter,
 
   readableDate: (dateObj) => {
-    return DateTime.fromJSDate(dateObj).toFormat('dd LLL yyyy');
+    return DateTime.fromJSDate(dateObj).toFormat("dd LLL yyyy")
   },
 
   htmlDateString: (dateObj) => {
-    return DateTime.fromJSDate(dateObj).toFormat('yyyy-LL-dd');
+    return DateTime.fromJSDate(dateObj).toFormat("yyyy-LL-dd")
   },
 
   formatDate: (dateObj, format) => {
-    return DateTime.fromJSDate(dateObj).toFormat(format);
+    return DateTime.fromJSDate(dateObj).toFormat(format)
   },
 
   limit: (arr, limit) => {
-    return arr.slice(0, limit);
+    return arr.slice(0, limit)
   },
 
   validTags: (tags) => {
     if (!tags) {
-      return [];
+      return []
     }
-    return withoutSpecialTags(tags).map((t) => slugFilter(t));
+    return withoutSpecialTags(tags).map((t) => slugFilter(t))
   },
 
-  withoutSpecialTags: withoutSpecialTags,
-};
+  withoutSpecialTags: withoutSpecialTags
+}
