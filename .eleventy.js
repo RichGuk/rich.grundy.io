2b5f68d37d5bdb904ef2244ebc5deaa56d7e3b80
Rich Grundy <rich@grundy.io>
2023-12-29 13:10:40 +0000

    Upgraded 11ty.

--- .eleventy.js
+++ .eleventy.js
@@ -1,60 +1,65 @@
-const shortcodes = require('./utils/shortcodes.js')
-const filters = require('./utils/filters.js')
-const collections = require('./utils/collections.js')
-const transforms = require('./utils/transforms.js')
+const shortcodes = require("./utils/shortcodes.js");
+const filters = require("./utils/filters.js");
+const collections = require("./utils/collections.js");
+const transforms = require("./utils/transforms.js");
 
-const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight')
-const pluginRss = require('@11ty/eleventy-plugin-rss')
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const pluginRss = require("@11ty/eleventy-plugin-rss");
+const UpgradeHelper = require("@11ty/eleventy-upgrade-help");
 
 module.exports = (eleventyConfig) => {
-  eleventyConfig.setDataDeepMerge(true)
+  eleventyConfig.addPlugin(UpgradeHelper);
+  eleventyConfig.setDataDeepMerge(true);
 
-  eleventyConfig.addPlugin(syntaxHighlight)
-  eleventyConfig.addPlugin(pluginRss)
+  eleventyConfig.addPlugin(syntaxHighlight);
+  eleventyConfig.addPlugin(pluginRss);
 
-  eleventyConfig.addLayoutAlias('default', 'layouts/pages.njk')
-  eleventyConfig.addLayoutAlias('pages', 'layouts/pages.njk')
+  eleventyConfig.addLayoutAlias("default", "layouts/pages.njk");
+  eleventyConfig.addLayoutAlias("pages", "layouts/pages.njk");
 
   eleventyConfig
-    .addPassthroughCopy({ 'src/assets/images': '/assets/images' })
-    .addPassthroughCopy({ 'src/assets/fonts': '/assets/fonts' })
-    .addPassthroughCopy({ 'src/assets/vendor': 'vendor' })
-    .addPassthroughCopy({ 'src/public': '/' })
+    .addPassthroughCopy({ "src/assets/images": "/assets/images" })
+    .addPassthroughCopy({ "src/assets/fonts": "/assets/fonts" })
+    .addPassthroughCopy({ "src/assets/vendor": "vendor" })
+    .addPassthroughCopy({ "src/public": "/" });
 
-  eleventyConfig.setBrowserSyncConfig({
-    ui: false,
-    ghostMode: true,
-    ignorePaths: ['ramblings/share/*'],
-    files: [
-      './dist/assets/css',
-      './dist/assets/js',
-    ]
-  })
+  // eleventyConfig.setBrowserSyncConfig({
+  //   ui: false,
+  //   ghostMode: true,
+  //   ignorePaths: ['ramblings/share/*'],
+  //   files: [
+  //     './dist/assets/css',
+  //     './dist/assets/js',
+  //   ]
+  // })
+  eleventyConfig.setServerOptions({
+    watch: ["./dist/assets/css", "./dist/assets/js"],
+  });
 
   Object.keys(shortcodes).forEach((name) => {
-    eleventyConfig.addShortcode(name, shortcodes[name])
-  })
+    eleventyConfig.addShortcode(name, shortcodes[name]);
+  });
 
   Object.keys(filters).forEach((name) => {
-    eleventyConfig.addFilter(name, filters[name])
-  })
+    eleventyConfig.addFilter(name, filters[name]);
+  });
 
   Object.keys(collections).forEach((name) => {
-    eleventyConfig.addCollection(name, collections[name])
-  })
+    eleventyConfig.addCollection(name, collections[name]);
+  });
 
   Object.keys(transforms).forEach((name) => {
-    eleventyConfig.addTransform(name, transforms[name])
-  })
+    eleventyConfig.addTransform(name, transforms[name]);
+  });
 
-  eleventyConfig.setUseGitIgnore(false)
+  eleventyConfig.setUseGitIgnore(false);
 
   return {
     dir: {
-      input: 'src',
-      output: 'dist'
+      input: "src",
+      output: "dist",
     },
-    htmlTemplateEngine: 'njk',
-    markdownTemplateEngine: 'njk'
-  }
-}
+    htmlTemplateEngine: "njk",
+    markdownTemplateEngine: "njk",
+  };
+};
