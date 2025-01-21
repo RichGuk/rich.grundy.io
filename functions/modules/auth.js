2fcc9bda19e930228a6209cde31c67505c94948a
Rich Grundy <rich@grundy.io>
2025-01-21 21:11:45 +0000

    Removing old functions (not used anymore).

--- functions/modules/auth.js
+++
@@ -1,6 +0,0 @@
-import { NetlifyJwtVerifier } from '@serverless-jwt/netlify'
-
-export const verifyJwt = NetlifyJwtVerifier({
-  issuer: process.env.AUTH0_ISSUER,
-  audience: process.env.AUTH0_AUDIENCE
-})
